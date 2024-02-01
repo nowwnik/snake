8916eab2211cccbd60fce4c403fc14c604d02118
nowwnik <nik.butorin.2018@bk.ru>
2024-02-01 19:58:22 +0500

    оно бегает

--- snake.py
+++ snake.py
@@ -1,57 +1,86 @@
+import time
+import random
 import pygame
 
-#colors
+# colors
 RED = (255, 0, 0)
 BackGroud = (220, 220, 220)
 dot = (48, 213, 200)
 
 pygame.init()
-HEIGHT, WEIGHT = 600, 400
-screen = pygame.display.set_mode((HEIGHT, WEIGHT))
+HEIGHT, WIDTH = 600, 400
+screen = pygame.display.set_mode((HEIGHT, WIDTH))
 
 pygame.display.set_caption('snake')
 
-font = pygame.font.SysFont(None,25)
-
-x, y = HEIGHT/2, WEIGHT/2
-
-x_move, y_move = 0, 0
+font = pygame.font.SysFont('Impact', 30)
 
 snake_move = 8
-snake_speed = 15
-
-def message (txt, color):
-    text = font.render(txt,False,color)
-    screen.blit(text, (HEIGHT/2,WEIGHT/2 ))
+snake_speed = 10
 
 clock = pygame.time.Clock()
 
-game = True
 
-while game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                x_move = -10
-                y_move = 0
-            elif event.key == pygame.K_d:
-                x_move = 10
-                y_move = 0
-            elif event.key == pygame.K_w:
-                y_move = -10
-                x_move = 0
-            elif event.key == pygame.K_s:
-                x_move = 10
-                y_move = 0
-    x += x_move
-    y += y_move
-    screen.fill(BackGroud)
+def message(txt, color):
+    text = font.render(txt, False, color)
+    screen.blit(text, [HEIGHT / 2 - 100, WIDTH / 2 - 100])
 
-    pygame.draw.rect(screen, RED, [x, y, 8, 8])
-    pygame.display.update()
-    clock.tick(30)
 
-pygame.quit()
-quit()
+def gameLoop():
+    game = True
+    game_close = False
+    x, y = HEIGHT / 2, WIDTH / 2
+    x_move, y_move = 0, 0
+
+    snake = []
+    snake_length = 1
+
+    dot_x = round(random.randrange(0, WIDTH - snake_move) / 10.0) * 10.0
+    dot_y = round(random.randrange(0, WIDTH - snake_move) / 10.0) * 10.0
+
+    while game:
+
+        while game_close == True:
+            screen.fill(BackGroud)
+            message("Змейка умерла!\nQ - выйти, Е - играть снова", RED)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        #game = False
+                        pygame.quit()
+                        quit()
+                    elif event.key == pygame.K_e:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    x_move = -snake_move
+                    y_move = 0
+                elif event.key == pygame.K_d:
+                    x_move = snake_move
+                    y_move = 0
+                elif event.key == pygame.K_w:
+                    y_move = -snake_move
+                    x_move = 0
+                elif event.key == pygame.K_s:
+                    y_move = snake_move
+                    x_move = 0
+
+        if x >= HEIGHT or x < 0 or y >= WIDTH or y < 0:
+            game_close = True
+
+        x += x_move
+        y += y_move
+        screen.fill(BackGroud)
+
+        pygame.draw.rect(screen, dot, [dot_x, dot_y, snake_move, snake_move])
+        pygame.draw.rect(screen, RED, [x, y, snake_move, snake_move])
+        pygame.display.update()
+        clock.tick(snake_speed)
+
+
+gameLoop()
